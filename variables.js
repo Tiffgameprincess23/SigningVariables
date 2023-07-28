0a3ac640a813677cf236be8dcdbb9dbd4426fb41
Tiffany Stamp <136510743+Tiffgameprincess23@users.noreply.github.com>
2023-07-28 09:05:23 -0600

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,11 @@
+let a = 5
+let b = 2
+
+let plus = a + b
+let subtraction = a - b
+let multiplication = a * b
+let division = a / b 
+
+let remainder = 17
+let divisor = 3
+let result = remainder % divisor
